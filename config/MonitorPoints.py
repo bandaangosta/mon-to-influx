7dab73009811bbdee7b0171f3dfabcbda2b21949
Jose Ortiz (monitor-amg) <jose.ortiz@alma.cl>
2019-10-05 20:00:36 +0000

    Adds MLs AMBIENT_TEMPERATURE monitor point

--- config/MonitorPoints.py
+++ config/MonitorPoints.py
@@ -26,6 +26,22 @@ monitorPoints = [
     {
         'abm'       : 'CentralLO',
         'abm_alias' : 'CLO',
+        'lru'       : 'ML_930008016217fc10',
+        'lru_alias' : 'ML1',
+        'mon'       : 'LASER_SIGNAL_FL_TEMP_MON',
+        'mon_alias' : 'LASER_SIGNAL_FL_TEMP_MON'
+    },
+    {
+        'abm'       : 'CentralLO',
+        'abm_alias' : 'CLO',
+        'lru'       : 'ML_930008016217fc10',
+        'lru_alias' : 'ML1',
+        'mon'       : 'AMBIENT_TEMPERATURE',
+        'mon_alias' : 'AMBIENT_TEMPERATURE'
+    },
+    {
+        'abm'       : 'CentralLO',
+        'abm_alias' : 'CLO',
         'lru'       : 'ML2_3000080205eba710',
         'lru_alias' : 'ML2',
         'mon'       : 'SIGNAL_PM_DC_10V',
@@ -47,4 +63,28 @@ monitorPoints = [
         'mon'       : 'SIGNAL_RED_PD_PWR_MON',
         'mon_alias' : 'SIGNAL_RED_PD_PWR_MON'
     },
+    {
+        'abm'       : 'CentralLO',
+        'abm_alias' : 'CLO',
+        'lru'       : 'ML2_3000080205eba710',
+        'lru_alias' : 'ML2',
+        'mon'       : 'LASER_SIGNAL_FL_TEMP_MON',
+        'mon_alias' : 'LASER_SIGNAL_FL_TEMP_MON'
+    },
+    {
+        'abm'       : 'CentralLO',
+        'abm_alias' : 'CLO',
+        'lru'       : 'ML2_3000080205eba710',
+        'lru_alias' : 'ML2',
+        'mon'       : 'AMBIENT_TEMPERATURE',
+        'mon_alias' : 'AMBIENT_TEMPERATURE'
+    },
+    {
+        'abm'       : 'WeatherStationController',
+        'abm_alias' : 'WeatherStationController',
+        'lru'       : 'MeteoCentral',
+        'lru_alias' : 'MeteoCentral',
+        'mon'       : 'TEMPERATURE',
+        'mon_alias' : 'TEMPERATURE'
+    }
 ]
